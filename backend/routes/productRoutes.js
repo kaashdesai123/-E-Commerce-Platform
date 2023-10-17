948b83503c3cfb915abe2d3b21997390fb78c0e3
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-10-16 21:16:35 -0700

    Create productRoutes.js

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Product = require('../models/product');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
